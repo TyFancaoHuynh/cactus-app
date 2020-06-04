a307086d9776af241498e7b881e6d9237010fc36
TyFancaoHuynh <hoangtienhuynh2701@gmail.com>
2020-06-04 10:04:03 +0700

    add about page

--- lib/pages/about/about_page.dart
+++ lib/pages/about/about_page.dart
@@ -2,14 +2,38 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "About",
-        style: TextStyle(color: Colors.black),
+    return Container(
+        child: ListView(
+            children: ListTile.divideTiles(context: context, tiles: [
+      ListTile(
+        title: Text(
+          "Thông tin ứng dụng",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
+        ),
       ),
-    );
+      ListTile(
+        title: Text("Title 1"),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+      ListTile(
+        title: Text("Title 2"),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+      ListTile(
+        title: Text("Title 3"),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+      ListTile(
+        title: Text("Title 4"),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+      ListTile(
+        title: Text("Title 5"),
+        trailing: Icon(Icons.arrow_forward_ios),
+      )
+    ]).toList()));
   }
 }
